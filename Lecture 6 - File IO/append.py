e14563f551c86b9b960dd9d19f0fef182a9ef40e
Igor Rafael <igorrafael.b27@gmail.com>
2026-05-04 15:48:16 -0300

    lecture-06: File I/O

---
+++ Lecture 6 - File IO/append.py
@@ -0,0 +1,8 @@
+import csv
+
+name = input("What's your name? ")
+home = input("What's your home? ")
+
+with open("names.csv", "a") as file:
+  writer = csv.DictWriter(file, fieldnames=["name", "home"])
+  writer.writerow({"name": name, "home": home})
